455976d647678db6a8ff05fed7362f9f5560e4ae
kyperbelt <jonathancamarenadev@gmail.com>
2023-01-31 21:17:56 -0800

    added movement tracking and slowing when standing still for two long

---
+++ docs/movement.md
@@ -0,0 +1,7 @@
+Wanted vroot to move slower the longer he stood still but I did not want it to be done in a linear fashion. I used desmos to look for a good curve to use and found this one: 
+
+$$f\left(x\right)\ =\ 1-\left(1\ -\ .9x\right)^{2}$$
+Which gives me the value between 0 and .99 for any value of x between 0 and 1. I then multiplied this value by the speed I wanted the vroot to move at and that gave me the speed I wanted the vroot to move at.
+
+The value of x is calculated by taking the time that vroot has been standing still dividided by the max time he can stand still.
+$$x\ =\ \frac{time\ standing\ still}{max\ time\ standing\ still}$$
--- src/vroot.gd
+++ src/vroot.gd
@@ -7,6 +7,10 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+# create a mechanic for when the player is standing still, the longer he stands still the slower he moves
+# until a max where he can't move at all
+var standing_still_elapsed:= 0.0
+var standing_still_max := 3.0
 
 
 func _physics_process(delta):
@@ -18,14 +22,28 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+	if velocity.y == 0:
+		if velocity.x == 0:
+			standing_still_elapsed += delta
+		else: 
+			standing_still_elapsed -= delta
+
+	standing_still_elapsed = clamp(standing_still_elapsed, 0, standing_still_max)
+	var standing_still_alpha = standing_still_elapsed/standing_still_max
+	$ProgressBar.ratio = standing_still_alpha
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		$icon.flip_h = direction < 0
-		velocity.x = direction * SPEED
+		velocity.x = direction * SPEED * (1.0 - get_movement_multiplier(standing_still_elapsed/standing_still_max))
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
 
 
 	move_and_slide()
+
+
+func get_movement_multiplier(alpha: float)->float:
+	return 1.0 - (1.0 - alpha * 0.9) * (1.0 - alpha * 0.9)
