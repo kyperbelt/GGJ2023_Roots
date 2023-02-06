b334f42ab100cf0377b81df97d30b8e0c6b8c123
Kyperbelt <kyperbelt@users.noreply.github.com>
2023-02-05 16:53:24 -0800

    end

---
+++ src/Cottage.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_area_entered(area:Area2D):
+	if "player" in area.get_groups():
+		self.get_tree().change_scene_to_file("res://src/TitleScreen.tscn")
+		pass
+	pass # Replace with function body.
